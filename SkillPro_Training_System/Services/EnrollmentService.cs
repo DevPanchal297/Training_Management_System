19b5061b24b4b6e24e5fc337283ddb706c9082c0
Dev Panchal <devpanchal1452@gmail.com>
2025-08-19 23:22:10 +0530

    Add project files.

---
+++ SkillPro_Training_System/Services/EnrollmentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillPro_Training_System.Services
+{
+    internal class EnrollmentService
+    {
+    }
+}
